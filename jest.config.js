5fee1bd0f1475dbda2fde4eab783781457a30f70
Ivan Zakharchanka <3axap4eHko@gmail.com>
2024-03-07 18:09:12 -0500

    switch to pnpm

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 export default {
   verbose: true,
-  testEnvironment: 'node',
   collectCoverage: !!process.env.CI,
   collectCoverageFrom: ['index.js', 'runner.js'],
-  coveragePathIgnorePatterns: ['/node_modules/', '__fixtures__', '__mocks__', '__tests__'],
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/coverage', '/node_modules/', '__tests__'],
   coverageDirectory: './coverage',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
   transform: {},
+  testMatch: ['**/__tests__/**/*.js'],
 };
